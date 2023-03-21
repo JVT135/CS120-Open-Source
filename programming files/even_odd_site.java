7d86ab65d557f968876ada50802abec9abd1a707
JVT135 <57076677+JVT135@users.noreply.github.com>
2023-03-21 16:39:50 -0400

    Update even_odd_site.java

--- programming files/even_odd_site.java
+++ programming files/even_odd_site.java
@@ -4,9 +4,15 @@ import java.io.*;
 
 public class even_odd_site {
     public static void main(String[] args) throws IOException {
+        
+        // Create a BufferedWriter Object
         BufferedWriter file = new BufferedWriter(new FileWriter("numbers.html"));
+        
+        // Write to a HTML file
         file.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n");
         file.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n");
+        
+        // Even-Odd calc and output to HTML
         for (int i = 1; i <= 50; i++) {
             if (i % 2 == 0) {
                 file.write("<tr><td>" + i + "</td><td></td></tr>\n");
@@ -17,7 +23,11 @@ public class even_odd_site {
         }
         file.write("</table>\n</body>\n</html>");
         file.close();
+        
+        // New Buffered Reader Object
         BufferedReader reader = new BufferedReader(new FileReader("numbers.html"));
+        
+        // Print HTML file to console
         String line;
         while ((line = reader.readLine()) != null) {
             System.out.println(line);
